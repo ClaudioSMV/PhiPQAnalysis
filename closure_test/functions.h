f8137bd532a7f3d53a10cad8cc83547f8f383f29
Claudio San Martin <claudio.sanmartinval@gmail.com>
2021-08-24 12:34:43 -0400

    add new 5d acceptance calculation with THnSparse

--- closure_test/functions.h
+++ closure_test/functions.h
@@ -7,5 +7,8 @@ int var_position(int Nvar, float var, std::vector<float> var_limits);
 int var_position(int i, int Nvar, std::vector<float> *var, float var_limits[]);
 int var_position(int i, int Nvar, std::vector<float> *var, std::vector<float> var_limits);
 
-void correct(float var,TH1F *hacc,TH1F *hcorr);
-void correctCSMV(float var_to_corr, float var_acc,TH1F *hacc,TH1F *hcorr);
+// void correct(float var,TH1F *hacc,TH1F *hcorr);
+void correct(float var_to_corr, float var_acc,TH1F *hacc,TH1F *hcorr);
+
+void setBinVarSize(THnSparse *hist, Int_t nbins[], Double_t Q2_limits[], Double_t Nu_limits[],
+					Double_t Zh_limits[], Double_t Pt2_limits[], Double_t PhiPQ_limits[]);
